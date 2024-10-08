7ab008e4d598d2422a9ec7a77de7e3658d33d274
Tola Voeung <tolacambo@gmail.com>
2024-10-08 15:38:05 +0700

    updated PassClef to AppKey

--- components/Loader.js
+++ components/Loader.js
@@ -1,6 +1,6 @@
 //
 //  Loader.js
-//  PassClef
+//  AppKey
 //
 //  Licensed to the Apache Software Foundation (ASF) under one
 //  or more contributor license agreements.  See the NOTICE file
--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,6 +1,6 @@
 //
 //  AuthContext.js
-//  PassClef
+//  AppKey
 //
 //  Licensed to the Apache Software Foundation (ASF) under one
 //  or more contributor license agreements.  See the NOTICE file
